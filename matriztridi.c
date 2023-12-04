aee5aa9900d8b1a39eacd3c1e641d57942318ce7
SantiagoPilamunga <santiago.pilamunga@udla.edu.ec>
2023-12-03 23:39:13 -0500

    correcciones

--- matriztridi.c
+++ matriztridi.c
@@ -3,33 +3,32 @@ void asignar(int l,int n,int m,int matriz[l][n][m])
 {
     for (int k = 0; k < l; k++)
     {
-        for (int i = 0; i < n; i++)
+        if (k<l)
+        {
+            for (int i = 0; i < n; i++)
         {
             for (int j = 0; j < m; j++)
             {
                 matriz[k][i][j]=0;
             } 
         }
-    }
-}
-void asignar2(int l,int n,int m,int matriz[l][n][m])
-{
-    for (int k = 0; k < l; k++)
-    {
-        for (int i = 0; i < n; i++)
+        }else if(k==l-1){
+            for (int i = 0; i < n; i++)
         {
             for (int j = 0; j < m; j++)
             {
                 matriz[k][i][j]=1;
             } 
         }
+        }
     }
-    
 }
+
 void mostrar(int l,int n,int m,int matriz[l][n][m])
 {
     for (int k = 0; k < l; k++)
     {
+        printf("Matriz %d\n",k+1);
         for (int i = 0; i < n; i++)
             {
                 for (int j = 0; j < m; j++)
@@ -50,15 +49,6 @@ int main()
     printf("Ingrese el mumero de columnas: ");
     scanf("%d", &m);
     int matrix[l][n][m];
-    for (int k = 0; k < l; k++)
-    {
-        if (k == l-1)
-        {
-            asignar2(l,n,m,matrix);
-        }else{
-            asignar(l,n,m,matrix);
-        }
-    }
     mostrar(l,n,m,matrix);
     return 0;
 }
